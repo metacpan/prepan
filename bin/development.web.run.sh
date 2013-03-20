c6b27e70c7fd6a329478af47482f40a617dbccea
Kentaro Kuribayashi <kentarok@gmail.com>
2013-03-21 01:02:37 +0900

    Fixed local development server port

--- bin/development.web.run.sh
+++ bin/development.web.run.sh
@@ -7,4 +7,4 @@ export APPROOT=/var/www/prepan
 cd $APPROOT || exit 1
 
 exec setuidgid deployer carton exec -- \
-     starman --port 8000 --workers=5 app.psgi
+     starman --port 8080 --workers=5 app.psgi
