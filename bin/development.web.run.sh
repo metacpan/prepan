9582522de3f2f0bbb44d897210ce472e66a9848b
shibayu36 <shibayu36@gmail.com>
2013-10-13 18:41:07 +0900

    specify accesslog file in development environment

--- bin/development.web.run.sh
+++ bin/development.web.run.sh
@@ -4,6 +4,7 @@ exec 2>&1
 export PATH=/usr/local/perl-prepan/bin:$PATH
 export PLACK_ENV=development
 export PREPAN_ENV=development
+export PREPAN_ACCESSLOG=/var/log/prepan/access.log
 export APPROOT=/var/www/prepan
 cd $APPROOT || exit 1
 
